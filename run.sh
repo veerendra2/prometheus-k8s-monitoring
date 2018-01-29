797892321040fd9904d0107c67c97d70010142ca
Ubuntu <ubuntu@ip-172-31-38-29.us-west-2.compute.internal>
2018-01-29 11:16:47 +0000

    display port info

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ kubectl apply -f cadvisor-daemonset.yml
 kubectl apply -f prometheus-configmap.yml
 kubectl apply -f prometheus-rbac.yml
 kubectl apply -f prometheus-deployment.yml
+echo ""
+kubectl get services | grep prometheus
