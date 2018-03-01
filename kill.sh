18e378caad645ecddf499d3e7967bf9337efad4f
Veerendra <veerendrav2@gmail.com>
2018-03-02 01:34:54 +0530

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl delete -f cadvisor-daemonset.yml
-kubectl delete -f prometheus-configmap.yml
-kubectl delete -f prometheus-rbac.yml
-kubectl delete -f prometheus-deployment.yml
+kubectl delete -f https://raw.githubusercontent.com/veerendra2/prometheus-k8s-monitoring/master/cadvisor-daemonset.yml
+kubectl delete -f https://raw.githubusercontent.com/veerendra2/prometheus-k8s-monitoring/master/prometheus-configmap.yml
+kubectl delete -f https://raw.githubusercontent.com/veerendra2/prometheus-k8s-monitoring/master/prometheus-rbac.yml
+kubectl delete -f https://raw.githubusercontent.com/veerendra2/prometheus-k8s-monitoring/master/prometheus-deployment.yml
