ddbf8bcfe8443e6b10cd593b6adcc61f6f17ac62
Veerendra <veerendrav2@gmail.com>
2018-03-02 01:34:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kubectl apply -f cadvisor-daemonset.yml
-kubectl apply -f prometheus-configmap.yml
-kubectl apply -f prometheus-rbac.yml
-kubectl apply -f prometheus-deployment.yml
+kubectl apply -f https://raw.githubusercontent.com/veerendra2/prometheus-k8s-monitoring/master/cadvisor-daemonset.yml
+kubectl apply -f https://raw.githubusercontent.com/veerendra2/prometheus-k8s-monitoring/master/prometheus-configmap.yml
+kubectl apply -f https://raw.githubusercontent.com/veerendra2/prometheus-k8s-monitoring/master/prometheus-rbac.yml
+kubectl apply -f https://raw.githubusercontent.com/veerendra2/prometheus-k8s-monitoring/master/prometheus-deployment.yml
 echo ""
 kubectl get services | grep prometheus
